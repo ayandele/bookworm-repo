2c0091ae254e7f1ab41f1dd657d1a895a1265e86
konawave <nickkjeffs@gmail.com>
2023-05-11 18:00:51 -0400

    Figured out how to render the book titles of a given keyword search onto the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,14 @@
-// fetch('https://www.googleapis.com/books/v1/volumes?q=search+terms&key=AIzaSyDPsiNcDrNWoAHFp1fGOwZtBuGMJRUchhs')
-//     .then(res => res.json())
-//     .then(data => console.log(data));
-
 fetch(`https://www.googleapis.com/books/v1/volumes?q=search+terms&key=AIzaSyDPsiNcDrNWoAHFp1fGOwZtBuGMJRUchhs`,{
     method: "GET",
     headers: {
         "Accept": "application/json"
     },
 })
-.then(function (response) {
-    return response.json();
+    .then(function (response) {
+        return response.json();
 })
-.then(function (data) {
-    console.log(data);
-    // let typeInput = ['Computers']
-    // for (i = 0; i < data.items.length; i++) {
-    //     let selectedLists = data.items[i].volumeInfo.categories;
-    //.lists.filter(list => list.list_name.includes("Fiction"))
-    //     console.log(selectedLists)
-        
-    // }
+    .then(function (data) {
+        console.log(data);
 });
 
 let userInput = 'Comics'
@@ -31,8 +20,19 @@ fetch(requestURL ,{
         "Accept": "application/json"
     },
 })
-.then(function (response) {
-    return response.json();
+    .then(function (response) {
+        return response.json();
 })
-.then(function(data) {
-    console.log(data);})
+    .then(function(data) {
+        console.log(data.items[0].volumeInfo.title);
+        // for (i = 0; i < data.length)
+        console.log(data.items.length)
+        for (i = 0; i < data.items.length; i++) {
+            let par = document.createElement('p');
+            let node = document.createTextNode(JSON.stringify(data.items[i].volumeInfo.title));
+            document.body.appendChild(par);
+            par.appendChild(node);
+            console.log(par);
+        }
+    })
+
