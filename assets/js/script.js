fc5e6f8ab482a565de9b932144466975baae5dca
Sydney Laub <syd.laub18@gmail.com>
2023-05-11 19:05:02 -0400

    nyt api changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,26 @@ fetch(`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${n
     // nonfiction list
     const nonFiction = data.results.lists.filter(list => list.list_name.includes("Nonfiction"))
     console.log(nonFiction)
+    
+    // code that displays the fiction book log
+    var fictionObj = {titles: [""]}
+    function displayFiction (){
+
+        // object to push all of the fiction titles into
+        
+        // for loop that gathers all of the fiction titles
+        for (i = 0; i < fictionList.length; i++){
+            // var fictionDivs = $("<div>")
+            // var fictionTitle = $("<h2>")
+            fictionTitle.text(fictionList[i].books[i].title)
+
+            fictionObj.push(fictionTitle)
+        }
+        console.log(fictionObj)
+    }
+
 });
-
-
-
 // need to dynamically render pertinent information to the page when a user searches for specific key words
+
 
 // need to create an event listener for each button the user can click that will get specific information from each API 
