b2cdab055d726e78274e6a7a0d14ae2e1c0d3222
konawave <nickkjeffs@gmail.com>
2023-05-11 21:26:18 -0400

    Thumbnails are now appearing properly! They just need to be resized and placed before the book name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
-fetch(`https://www.googleapis.com/books/v1/volumes?q=search+terms&key=AIzaSyDPsiNcDrNWoAHFp1fGOwZtBuGMJRUchhs`,{
-    method: "GET",
-    headers: {
-        "Accept": "application/json"
-    },
-})
-    .then(function (response) {
-        return response.json();
-})
-    .then(function (data) {
-        console.log(data);
-});
+const key = `https://www.googleapis.com/books/v1/volumes?q=search+terms&key=AIzaSyDPsiNcDrNWoAHFp1fGOwZtBuGMJRUchhs`
 
-let userInput = 'Comics'
-let requestURL = 'https://www.googleapis.com/books/v1/volumes?q=' + userInput + '&startIndex=20&maxResults=40'
+
+let userInput = 'Cooking'
+let requestURL = 'https://www.googleapis.com/books/v1/volumes?q=' + userInput + '&maxResults=15'
 
 fetch(requestURL ,{
     method: "GET",
@@ -24,15 +14,28 @@ fetch(requestURL ,{
         return response.json();
 })
     .then(function(data) {
-        console.log(data.items[0].volumeInfo.title);
+        console.log(data)
         // for (i = 0; i < data.length)
         console.log(data.items.length)
         for (i = 0; i < data.items.length; i++) {
-            let par = document.createElement('p');
-            let node = document.createTextNode(JSON.stringify(data.items[i].volumeInfo.title));
-            document.body.appendChild(par);
-            par.appendChild(node);
-            console.log(par);
+            // let par = document.createElement('p');
+            // let node = document.createTextNode(JSON.stringify(data.items[i].volumeInfo.imageLinks.thumbnail));
+            // document.querySelector('.list-group').appendChild(par)
+            // par.appendChild(node);
+            // console.log(par);
+            
+            if (data.items[i].volumeInfo.imageLinks != undefined) {
+                let img = document.createElement('img')
+                img.setAttribute('src', data.items[i].volumeInfo.imageLinks.thumbnail);
+                document.querySelector('.list-group').appendChild(img);}
+            else {
+                let par = document.createElement('p')
+                let node = document.createTextNode('Image unavailable');
+                document.querySelector('.list-group').appendChild(par);
+                par.appendChild(node);
+            }
+
+            
         }
     })
 
